cf4aca4a974eb95e904b61309eb1603000ef761e
Neilson Ramalho <neilsoncarlos@gmail.com>
2014-12-21 22:40:21 -0200

    new methods for double linked lists

--- DoubleLList/src/main/java/com/neilson/lists/DList.java
+++ DoubleLList/src/main/java/com/neilson/lists/DList.java
@@ -1,7 +1,87 @@
 package com.neilson.lists;
+
 /**
  * Created by neilson on 12/21/14.
  */
 public class DList {
+    public Node head;
 
+    public int countElements() {
+        int count = 0;
+        if (head != null) {
+            Node temp = head;
+            while (temp != null) {
+                count++;
+                temp = temp.right;
+            }
+        }
+        return count;
+    }
+
+    public void insertHead(int value) {
+        if (head == null) {
+            head = new Node(value);
+            return;
+        }
+        Node node = new Node(value);
+        node.right = head;
+        head.left = node;
+        head = node;
+    }
+
+    public int [] comeAndGo() {
+        int[] completeTurn = new int[2 * countElements() - 1];
+        int index = 0;
+        if (head != null) {
+            Node temp = head;
+            while (temp.right != null) {
+                completeTurn[index] = temp.value;
+                index++;
+                temp = temp.right;
+            }
+            completeTurn[index] = temp.value;
+            index++;
+            temp = temp.left;
+            while (temp != null) {
+                completeTurn[index] = temp.value;
+                index++;
+                temp = temp.left;
+            }
+        }
+        return completeTurn;
+    }
+
+    public void insertTail(int value) {
+        if (head == null) {
+            head = new Node(value);
+            return;
+        }
+        Node temp = head;
+        while (temp.right != null) {
+            temp = temp.right;
+        }
+        Node newNode = new Node(value);
+        temp.right = newNode;
+        newNode.left = temp;
+    }
+
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.right;
+        }
+    }
+
+    public int[] toArray() {
+        int[] array = new int[countElements()];
+        Node temp = head;
+        int index = 0;
+        while (temp != null) {
+            array[index] = temp.value;
+            temp = temp.right;
+            index++;
+        }
+        return array;
+    }
 }
--- DoubleLList/src/test/java/com/neilson/lists/DLListTest.java
+++ DoubleLList/src/test/java/com/neilson/lists/DLListTest.java
@@ -1,14 +1,53 @@
 package com.neilson.lists;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by neilson on 12/21/14.
  */
 public class DLListTest {
+
     @Test
-    public void insertList(){
-        assertEquals(1, 1);
+    public void testCount() {
+        DList list = new DList();
+        insertNElements(list, 5);
+        assertEquals(5, list.countElements());
+    }
+
+    @Test
+    public void insertHead() {
+        DList list = new DList();
+        list.insertHead(1);
+        list.insertHead(2);
+        list.insertHead(3);
+        assertArrayEquals(new int[]{3, 2, 1}, list.toArray());
+    }
+
+    @Test
+    public void insertTail() {
+        DList list = new DList();
+        list.insertTail(1);
+        list.insertTail(2);
+        list.insertTail(3);
+        assertArrayEquals(new int[]{1, 2, 3}, list.toArray());
+    }
+
+    @Test
+    public void comeAndGo() {
+        DList list = new DList();
+        list.insertTail(1);
+        list.insertTail(2);
+        list.insertTail(3);
+        list.insertTail(4);
+        assertArrayEquals(new int[]{1,2,3,4,3,2,1}, list.comeAndGo());
+    }
+
+    public void insertNElements(DList list, int numElements) {
+        for (int i = numElements; i > 0; i--) {
+            list.insertHead(i);
+        }
     }
 }
